0fde53a85551c876f8f0c98716615313a4027515
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-05-13 13:59:28 +0530

    feat: added primary color

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -38,7 +38,7 @@ export default function HomePage() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 to="/Auth"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-700"
               >
                 Get started
               </Link>
